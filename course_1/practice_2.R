2bcb6371b80f059579b4e58b5a35f7a113f37bcb
Sophiasufas <b04701140@ntu.edu.tw>
2018-03-05 02:15:18 +0800

    text

--- course_1/practice_2.R
+++ course_1/practice_2.R
@@ -3,25 +3,25 @@
 
 
 # Create a vector called course.students.number, with data: c(1, 30)
-course.student.number <- 
+course.student.number <- c(1,30)
 
 # Create a variable csn, with data: length of course.student.number
-csn <- 
+csn <- length of course.student.number
 
 # Create a vector course.student.grade, with sample() function: x = c(55:100), size = csn
-course.student.grade <- 
+course.student.grade <- sample(x = c(55:100), size=csn)
 
 # Assign course.student.number as names of the course.student.grade
-names(course.student.grade) <-
+names(course.student.grade) <- course.student.number
 
 # Create csg.mean, with the mean value of course.student.grade
-csg.mean <- 
+csg.mean <- mean(course.student.grade)
   
 # Create csg.max with the max value of course.student.grade
-csg.max <- 
+csg.max <- max(course.student.grade)
 
 # Create csg.min with the min value of course.student.grade
-csg.min <- 
+csg.min <- min(course.student.grade)
 
 # Create a vector csg.over.80, with the logical result of course.student.grade over 80
 csg.over.80 <- 
