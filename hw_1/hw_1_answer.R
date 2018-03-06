3a38414cf0725a9714197a0d3dbfbf876bc9e76e
Sophiasufas <b04701140@ntu.edu.tw>
2018-03-07 01:17:31 +0800

    text

--- hw_1/hw_1_answer.R
+++ hw_1/hw_1_answer.R
@@ -7,19 +7,19 @@
 iris
 
 # 使用dim(), 回傳iris的列數與欄數
-
+dim(iris)
 
 # 使用head() 回傳iris的前六列
-
+head(iris)
 
 # 使用tail() 回傳iris的後六列
-
+tail(iris)
 
 # 使用str() 
-
+str(iris)
 
 # 使用summary() 查看iris敘述性統計、類別型資料概述。
-
+summary(iris)
 
 ########################################################### Task 2
 
