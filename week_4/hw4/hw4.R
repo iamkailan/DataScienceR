6289e3d22029f81ccf5e5841379c66238f4b57ff
Sophiasufas <b04701140@ntu.edu.tw>
2018-04-12 10:34:50 +0800

    update

--- week_4/hw4/hw4.R
+++ week_4/hw4/hw4.R
@@ -6,6 +6,9 @@ library(jiebaR)
 library(RColorBrewer)
 library(wordcloud)
 
-token <-"EAACEdEose0cBANNQvF3w8KoVLeIo2vFfZAxdSz2OFsN9tGeoX7AksqYdG6hUEO4uDM1KugVs9h5w6vFADyhZBLudWfwzcDJcIfOxpqVwsjg9C1CZABQAMpx0ZCIkzgL5JNIjgMBzRZBIpPRWiJtUZCrWrbqM2DnudrK6bBquJsnPZAyZBRk2Mguz6bByjtOno3oDLqOgBnB0eQZDZD"
+token <-"EAACEdEose0cBAKMbgO5ZBLStLpYYscaKTy3PglgNyXYn3msmuIbr4BPPmln6BzIL7D5yAy5iM7fZAbSYgj3J8RTZAFPZBDzzI2ue7ebr6Td3Glz6GqtYevNVmIAWpjuZC7kXcJxldGeHplx5UwiuISlxSbA50uZCZAjf9wxQ7sLqUrxyXycuzrVXt3OfPrK4wZBHznJGjAF5yQZDZD"
 me <- getUsers("me", token, private_info = TRUE)
 me$name
+page.id <- "445164788956922" 
+page <- getPage(page.id, token, n = 300)
+str(page)
